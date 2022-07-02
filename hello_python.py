2f76aa8388dea46c19611508454a03916e478260
ashutoshpydev <108576569+ashutoshpydev@users.noreply.github.com>
2022-07-02 13:21:36 +0000

    Add files via upload
    Different method for printing hello world in python programming language.

---
+++ hello_python.py
@@ -0,0 +1,23 @@
+#Different method for printing hello world.
+#print() funtion used to directly print output on the consol/terminal/cmd.
+
+#1. using simple print statement
+print("hello_python")
+
+
+#2. using a variable 
+
+output="hello_python"
+print(output)
+
+#3. using string formating method f
+
+print(f"{output}")
+
+#4. using string formatting method format()
+
+print("{0}".format(output))
+
+#5. using string formatting method %
+
+print("%s"%(output))
